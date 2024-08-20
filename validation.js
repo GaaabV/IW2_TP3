1843ca3d59a27c860a8b746b20f2c8c140a8fdfe
Gabv <gab.villeneuve@outlook.comgit config --global user.name>
2024-08-19 22:00:04 -0400

    validation js et ajouts pages

--- validation.js
+++ validation.js
@@ -2,18 +2,41 @@ const form = document.getElementById('form');
 const email = document.getElementById('email');
 
 const validateForm = () => {
-
     let noError = true;
     const emailValue = email.value.trim();
+
+    if (emailValue === '') {
+        setError(email, 'Email is required');
+        noError = false;
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+        noError = false;
+    } else {
+        setSuccess(email);
+    }
+
+    return noError;
 }
 
-if (emailValue === '') {
-    setError(email, 'Email is required');
-    noError = false;
-} else if (!isValidEmail(emailValue)) {
-    setError(email, 'Provide a valid email address');
-    noError = false;
-}
-else {
-    setSuccess(email);
-}
+const isValidEmail = email => {
+    const re =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.errorMessage');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}; 
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.errorMessage');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
